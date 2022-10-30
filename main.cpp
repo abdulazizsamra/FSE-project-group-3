edad40e3f0702c99247e19b8eb02c0295e8fb502
Polina Tarantsova <momandi@bk.ru>
2022-10-30 23:07:14 +0300

    Simplify shecks input

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "src/dfs.h"
 
 int main() {
-    std::cerr << "Enter the number of vertices and edges: ";
+    std::cout << "Enter the number of vertices and edges: ";
     int n, m;
     std::cin >> n >> m;
     while(true)
@@ -14,26 +14,23 @@ int main() {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
             std::cerr << "Vertices and edges are integer numbers."<<std::endl;
-            std::cerr << "Enter the number of vertices and edges: ";
-            std::cin >> n >> m;
+            exit(1);
         } else if (n < 1 || m < 0) {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
             std::cerr << "Number of edges must be >= 0, number of vertices must be >= 1."<<std::endl;
-            std::cerr << "Enter the number of vertices and edges: ";
-            std::cin >> n >> m;
+            exit(1);
         } else if (m > n * (n - 1) / 2){
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
             std::cerr << "Number of edges can not be more than n * (n - 1) / 2. Where n is numer of vertices."<<std::endl;
-            std::cerr << "Enter the number of vertices and edges: ";
-            std::cin >> n >> m;
+            exit(1);
         } else {
             break;
         }
     }
-    std::cerr << std::endl;
-    std::cerr << "Enter the graph in format parent_node child_node." << std::endl;
+    std::cout << std::endl;
+    std::cout << "Enter the graph in format parent_node child_node." << std::endl;
     std::vector<Node> nodes;
     for (int i = 0; i < n; ++i) {
         Node node;
@@ -49,27 +46,27 @@ int main() {
             {
                 std::cin.clear();
                 std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+                std::cerr << "ERROR in line " << i << " of graph input." << std::endl;
                 std::cerr << "Vertices must be integer numbers"<<std::endl;
-                std::cerr << "Reenter parent_node child_node: ";
-                std::cin >> parent >> child;
+                exit(1);
             } else if (parent < 0 || child < 0) {
                 std::cin.clear();
                 std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+                std::cerr << "ERROR in line " << i << " of graph input." << std::endl;
                 std::cerr << "Node id can not be less than 0."<<std::endl;
-                std::cerr << "Reenter parent_node child_node: ";
-                std::cin >> parent >> child;
+                exit(1);
             } else if (parent > n - 1 || child > n - 1) {
                 std::cin.clear();
                 std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+                std::cerr << "ERROR in line " << i << " of graph input." << std::endl;
                 std::cerr << "Node id can not be more than n - 1. Where n is number of nodes in the graph."<<std::endl;
-                std::cerr << "Reenter parent_node child_node: ";
-                std::cin >> parent >> child;
+                exit(1);
             } else if (parent == child) {
                 std::cin.clear();
                 std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+                std::cerr << "ERROR in line " << i << " of graph input." << std::endl;
                 std::cerr << "A node can't be parent to itself."<<std::endl;
-                std::cerr << "Reenter parent_node child_node: ";
-                std::cin >> parent >> child;
+                exit(1);
             } else {
                 break;
             }
